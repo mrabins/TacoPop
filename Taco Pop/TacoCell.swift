45b3d59e89a09560019f4dc4244cea6188a158b5
Mark Rabins <markrabins@gmail.com>
2016-12-28 12:15:41 -0800

    Set up DataService and UI of Tacos

---
+++ Taco Pop/TacoCell.swift
@@ -0,0 +1,18 @@
+//
+//  TacoCell.swift
+//  Taco Pop
+//
+//  Created by Mark Rabins on 12/28/16.
+//  Copyright © 2016 self.edu. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
